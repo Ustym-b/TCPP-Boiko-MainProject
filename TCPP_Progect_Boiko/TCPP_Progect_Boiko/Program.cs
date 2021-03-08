ce0050590c9784a0be1c06160e46549c90dc70c7
Ustym-b <ystim2314@gmail.com>
2021-03-08 09:23:43 +0200

    First commit

---
+++ TCPP_Progect_Boiko/TCPP_Progect_Boiko/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPP_Progect_Boiko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
