7fab60fff9d1d2c6fcc982aaf9fd4592a84fdc5e
Ustym-b <ystim2314@gmail.com>
2021-03-09 13:18:46 +0200

    from IDE

--- TCPP_Progect_Boiko/TCPP_Progect_Boiko/Program.cs
+++ TCPP_Progect_Boiko/TCPP_Progect_Boiko/Program.cs
@@ -6,7 +6,7 @@ namespace TCPP_Progect_Boiko
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from IDE!");
         }
     }
 }
